ba3ee800518defebc5b38fadd13bceb89d6ac247
JohnBeebop <lazarenkov88@gmail.com>
2022-06-01 18:26:16 +0300

    Finished angry smile

--- lab3/3_draw.py
+++ lab3/3_draw.py
@@ -7,13 +7,6 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
-"""rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-                               """
 rect(screen, (172, 158, 158), (0, 0, 400, 400))
 circle(screen, (255, 255, 0), (200, 200), 100)
 circle(screen, (0, 0, 0), (200, 200), 100, 2)
@@ -26,6 +19,11 @@ circle(screen, (0, 0, 0), (150, 170), 23, 1)
 circle(screen, (255, 37, 31), (250, 170), 17)
 circle(screen, (0, 0, 0), (250, 170), 10)
 circle(screen, (0, 0, 0), (250, 170), 17, 1)
+
+polygon(screen, (0, 0, 0), [(230, 160), (225, 150),
+                            (300, 125), (305, 135)])
+polygon(screen, (0, 0, 0), [(90, 115), (95, 105),
+                            (185, 150), (180, 160)])
 
 pygame.display.update()
 clock = pygame.time.Clock()
